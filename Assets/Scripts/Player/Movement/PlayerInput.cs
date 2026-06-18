a6656aa4ee6d2ebe1dfc1780bbe6f94d36a51efd
Muhammad Hasham <m.hasham1239@gmail.com>
2026-06-18 19:09:16 +0500

    feature/Included-Crouching and Sprinting and refactor to Single Principality

---
+++ Assets/Scripts/Player/Movement/PlayerInput.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Player.Movement
+{
+    public class PlayerInput : MonoBehaviour
+    {
+        public float HorizontalInput { get; private set; }
+        public float VerticalInput { get; private set; }
+        public bool JumpPressed { get; private set; }
+        public bool SprintHeld { get; private set; }
+        public bool CrouchHeld { get; private set; }
+
+        private void Update()
+        {
+            HorizontalInput = Input.GetAxisRaw("Horizontal");
+            VerticalInput = Input.GetAxisRaw("Vertical");
+
+            if (Input.GetKeyDown(KeyCode.Space))
+                JumpPressed = true;
+            
+            SprintHeld = Input.GetKey(KeyCode.LeftShift);
+            CrouchHeld = Input.GetKey(KeyCode.LeftControl);
+        }
+
+        public void ResetJump()
+        {
+            JumpPressed = false;
+        }
+    }
+}
--- Assets/Scripts/Player/Movement/PlayerMovement.cs
+++ Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -2,46 +2,58 @@ using UnityEngine;
 
 namespace Player.Movement
 {
-    [RequireComponent(typeof(Rigidbody))]
+    [RequireComponent(typeof(Rigidbody), typeof(PlayerInput), typeof(GroundDetector))]
     public class PlayerMovement : MonoBehaviour
     {
         [Header("References")]
         [SerializeField] private Transform orientation;
 
         [Header("Movement Settings")]
-        [SerializeField] private float moveSpeed = 10f;
+        [SerializeField] private float walkSpeed = 6f;
+        [SerializeField] private float sprintSpeed = 10f;
+        [SerializeField] private float crouchSpeed = 3f;
         [SerializeField] private float jumpForce = 5f;
         [SerializeField] private float airMultiplier = 0.5f;
-        
-        [Header("Ground Check")]
-        [SerializeField] private float playerHeight = 2f;
-        [SerializeField] private float groundCheckDistance = 0.2f;
-        [SerializeField] private LayerMask groundMask;
 
-        [Header("Drag Settings")]
-        [SerializeField] private float groundDrag = 0.5f;
-        [SerializeField] private float airDrag = 0.75f;
+        [Header("Crouch Settings")]
+        [SerializeField] private float crouchYScale = 0.5f;
+        private float _startYScale;
         
-        [Header("Speed Limits")]
-        [SerializeField] private float maxSpeed = 6f;
+        [Header("Drag Settings")]
+        [SerializeField] private float groundDrag = 5f;
+        [SerializeField] private float airDrag = 0f;
         
         private Rigidbody _rb;
-        private CapsuleCollider _col;
-        private float _horizontalInput;
-        private float _verticalInput;
-        private bool _isGrounded;
-        private bool _jumpPressed;
+        private PlayerInput _input;
+        private GroundDetector _groundDetector;
+
+        private float _moveSpeed;
+
+        public enum MovementState
+        {
+            Walking,
+            Sprinting,
+            Crouching,
+            Air
+        }
+
+        public MovementState state;
         
         private void Awake()
         {
             _rb = GetComponent<Rigidbody>();
-            _col = GetComponentInChildren<CapsuleCollider>();
+            _input = GetComponent<PlayerInput>();
+            _groundDetector = GetComponent<GroundDetector>();
+
+            _startYScale = transform.localScale.y;
+            
+            // Set initial damping
+            _rb.freezeRotation = true;
         }
 
         private void Update()
         {
-            ReadInput();
-            CheckGround();
+            StateHandler();
             ApplyDrag();
         }
 
@@ -52,40 +64,69 @@ namespace Player.Movement
             LimitSpeed();
         }
 
-        private void ReadInput()
+        private void StateHandler()
         {
-            _horizontalInput = Input.GetAxisRaw("Horizontal");
-            _verticalInput = Input.GetAxisRaw("Vertical");
-            
-            if (Input.GetKeyDown(KeyCode.Space))
-                _jumpPressed = true;
+            // Mode - Crouching
+            if (_input.CrouchHeld)
+            {
+                state = MovementState.Crouching;
+                _moveSpeed = crouchSpeed;
+                transform.localScale = new Vector3(transform.localScale.x, crouchYScale, transform.localScale.z);
+            }
+            else
+            {
+                transform.localScale = new Vector3(transform.localScale.x, _startYScale, transform.localScale.z);
+                
+                // Mode - Sprinting
+                if (_groundDetector.IsGrounded && _input.SprintHeld)
+                {
+                    state = MovementState.Sprinting;
+                    _moveSpeed = sprintSpeed;
+                }
+                // Mode - Walking
+                else if (_groundDetector.IsGrounded)
+                {
+                    state = MovementState.Walking;
+                    _moveSpeed = walkSpeed;
+                }
+                // Mode - Air
+                else
+                {
+                    state = MovementState.Air;
+                }
+            }
         }
         
         private void MovePlayer()
         {
-            Vector3 moveDirection =
-                orientation.forward * _verticalInput +
-                orientation.right * _horizontalInput;
+            Vector3 moveDirection = orientation.forward * _input.VerticalInput + orientation.right * _input.HorizontalInput;
 
-            _rb.AddForce(
-                moveDirection.normalized * (moveSpeed ),
-                ForceMode.Force
-            );
-        }
-        
-        private void CheckGround()
-        {
-            _isGrounded = Physics.Raycast(
-                transform.position,
-                Vector3.down,
-                playerHeight * 0.5f + groundCheckDistance,
-                groundMask
-            );
+            // On Slope
+            if (_groundDetector.OnSlope)
+            {
+                _rb.AddForce(_groundDetector.GetSlopeMoveDirection(moveDirection) * (_moveSpeed * 20f), ForceMode.Force);
+
+                if (_rb.linearVelocity.y > 0)
+                    _rb.AddForce(Vector3.down * 80f, ForceMode.Force);
+            }
+            // Grounded
+            else if (_groundDetector.IsGrounded)
+            {
+                _rb.AddForce(moveDirection.normalized * (_moveSpeed * 10f), ForceMode.Force);
+            }
+            // In Air
+            else
+            {
+                _rb.AddForce(moveDirection.normalized * (_moveSpeed * 10f * airMultiplier), ForceMode.Force);
+            }
+
+            // Turn off gravity while on slope to avoid sliding
+            _rb.useGravity = !_groundDetector.OnSlope;
         }
         
         private void JumpPlayer()
         {
-            if (_jumpPressed && _isGrounded)
+            if (_input.JumpPressed && _groundDetector.IsGrounded)
             {
                 Vector3 velocity = _rb.linearVelocity;
                 velocity.y = 0;
@@ -94,47 +135,34 @@ namespace Player.Movement
                 _rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             }
 
-            _jumpPressed = false;
+            _input.ResetJump();
         }
         
         private void ApplyDrag()
         {
-            _rb.linearDamping = _isGrounded ? groundDrag : airDrag;
+            _rb.linearDamping = _groundDetector.IsGrounded ? groundDrag : airDrag;
         }
         
         private void LimitSpeed()
         {
-            Vector3 velocity = _rb.linearVelocity;
-
-            Vector3 flatVelocity = new Vector3(velocity.x, 0f, velocity.z);
-
-            float maxSpeed = this.maxSpeed;
-
-            if (flatVelocity.sqrMagnitude > maxSpeed * maxSpeed)
+            // Limit speed on slope
+            if (_groundDetector.OnSlope)
             {
-                Vector3 limited = flatVelocity.normalized * maxSpeed;
-
-                _rb.linearVelocity = new Vector3(
-                    limited.x,
-                    velocity.y,
-                    limited.z
-                );
+                if (_rb.linearVelocity.magnitude > _moveSpeed)
+                    _rb.linearVelocity = _rb.linearVelocity.normalized * _moveSpeed;
             }
-        }
-        
-        private void OnDrawGizmos()
-        {
-            if (_col == null) return;
+            // Limit speed on ground or in air
+            else
+            {
+                Vector3 velocity = _rb.linearVelocity;
+                Vector3 flatVelocity = new Vector3(velocity.x, 0f, velocity.z);
 
-            Gizmos.color = _isGrounded ? Color.green : Color.red;
-
-            Vector3 origin = transform.position;
-            Vector3 direction = Vector3.down;
-            float distance = playerHeight * 0.5f + groundCheckDistance;
-
-            Gizmos.DrawLine(origin, origin + direction * distance);
-            Gizmos.DrawWireSphere(origin + direction * distance, 0.1f);
-        }
-        
+                if (flatVelocity.magnitude > _moveSpeed)
+                {
+                    Vector3 limited = flatVelocity.normalized * _moveSpeed;
+                    _rb.linearVelocity = new Vector3(limited.x, velocity.y, limited.z);
+                }
+            }
+        }   
     }
 }
